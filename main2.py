f6368094d32ccc50f0a4d2cb90f86e6d461683dc
Zots <Ivan.Zots@nfl.com>
2026-02-10 14:50:54 -0500

    First commit

---
+++ main2.py
@@ -0,0 +1,35 @@
+import pyautogui, sys, time, random, pynput
+
+last_activity = time.time()
+automatedInput = False
+mouse_listener = pynput.mouse.Listener()
+keyboard_listener = pynput.keyboard.Listener()
+
+def mouseMove():
+
+    try:
+        global automatedInput
+        automatedInput = True
+        pyautogui.dragTo(random.randint(0, 1920), random.randint(0, 1200), 2, pyautogui.easeInBounce)
+        pyautogui.keyDown('shift')
+        pyautogui.keyUp('shift')
+        pyautogui.press('left', presses=2, interval=0.5)
+        pyautogui.press('right', presses=2, interval=0.5)
+        automatedInput = False
+    except KeyboardInterrupt:
+        sys.exit(0)
+
+def main():
+    keyboard_listener.start()
+    mouse_listener.start()
+    while True:
+        try:
+            if (time.time() - last_activity) > 240:
+                mouseMove()
+                global last_activity
+                last_activity = time.time()
+            time.sleep(30)
+        except KeyboardInterrupt:
+            sys.exit(0)
+
+main()
